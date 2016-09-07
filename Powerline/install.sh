b53ac0c2c00c2e1fa17079b0a394429f274fd306
bananaappletw <webberapple@gmail.com>
2016-09-07 05:35:55 -0700

    Add script

--- Powerline/install.sh
+++ Powerline/install.sh
@@ -2,3 +2,7 @@
 sudo apt-get install -y powerline
 sudo apt-get install -y python3-pip
 sudo pip3 install powerline-status
+DIR=$(dirname "$(readlink -f "$0")")
+cat $DIR/.vimrc | tee --append ~/.vimrc
+cat $DIR/.bashrc | tee --append ~/.bashrc
+cp $DIR/.tmux.conf ~/.tmux.conf
