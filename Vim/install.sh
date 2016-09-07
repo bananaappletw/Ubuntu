9aba39e9614ae53c80e49e1429a71d38fcdce10e
bananaappletw <webberapple@gmail.com>
2016-09-07 05:38:59 -0700

    Add script

--- Vim/install.sh
+++ Vim/install.sh
@@ -3,6 +3,8 @@ sudo apt-get install -y vim
 sudo apt-get install -y build-essential cmake
 sudo apt-get install -y python-dev python3-dev
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+DIR=$(dirname "$(readlink -f "$0")")
+cp $DIR/.vimrc ~/.vimrc
 vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 ./install.py --all
