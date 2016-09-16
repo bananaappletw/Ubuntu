b8acf2d40592b0ab8d9e7333e026c5d7f421c617
bananaappletw <webberapple@gmail.com>
2016-09-16 15:04:02 +0800

    Update nvm

--- Hexo/install.sh
+++ Hexo/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
+export NVM_DIR="/home/apple/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install stable
 sudo apt-get install -y nodejs-legacy
 sudo apt-get install -y npm
