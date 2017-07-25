ec59a2e06a84f6a0ec2b91c37a60213da5bf97ee
bananaappletw <bananaappletw@gmail.com>
2017-07-25 20:11:27 +0800

    Remove unnecessary script

--- Hexo/install.sh
+++ Hexo/install.sh
@@ -3,7 +3,5 @@ wget -qO- https://raw.github.com/creationix/nvm/master/install.sh | sh
 export NVM_DIR="/home/apple/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install stable
-sudo apt-get install -y nodejs-legacy
-sudo apt-get install -y npm
 npm install hexo-cli -g
 
