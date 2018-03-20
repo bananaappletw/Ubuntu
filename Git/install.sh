cd8230735fc1e95e27ea74ab03c4f09f22fa638a
bananaappletw <bananaappletw@gmail.com>
2018-03-20 16:33:16 +0800

    Fix error

--- Git/install.sh
+++ Git/install.sh
@@ -2,7 +2,9 @@
 sudo apt-get install -y git
 DIR=$(dirname "$(readlink -f "$0")")
 cp $DIR/.gitconfig ~/.gitconfig
-sudo snap install --classic go
+sudo add-apt-repository ppa:longsleep/golang-backports
+sudo apt-get update
+sudo apt-get install golang-go
 git clone https://github.com/github/hub.git /tmp/hub
 cd /tmp/hub
 script/build -o ~/bin/hub
